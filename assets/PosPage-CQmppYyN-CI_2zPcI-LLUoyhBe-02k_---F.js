a8f77ef3f7d5c1bc82efdfd3bdffb94c68a09099
tronstheking <javierenriquetrons@gmail.com>
2026-07-26 23:27:20 -0400

    fix: restaurar index.html fuente, eliminar archivos estaticos duplicados de root y deshabilitar venta rapida

--- assets/PosPage-CQmppYyN-CI_2zPcI-LLUoyhBe-02k_---F.js
+++
@@ -1,1 +0,0 @@
-import{g as r,l as t,D as a}from"./index-NMe6pjNJ.js";const p=()=>{const e=r(s=>s.openModal),o=t();return a.useEffect(()=>{e("pos"),o("/",{replace:!0})},[e,o]),null};export{p as PosPage};
