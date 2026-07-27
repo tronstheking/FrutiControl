753823e0cd10493793d549a87ac10efe4003e12a
tronstheking <javierenriquetrons@gmail.com>
2026-07-26 22:09:38 -0400

    Updates

--- assets/chevron-right-DNT8wVdj-CXU1T1sl-CqNjjzSd-CCJV0Qfa-CDVBWR23-DZpesEp8.js
+++ assets/chevron-right-DNT8wVdj-CXU1T1sl-CqNjjzSd-CCJV0Qfa-CDVBWR23-DZpesEp8.js
@@ -1,6 +1,6 @@
-import{K as o}from"./index-pr6zRv1q.js";/**
+import{K as h}from"./index-BbXNXHta.js";/**
  * @license lucide-react v0.417.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const h=o("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as o};
+ */const o=h("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{o as h};
