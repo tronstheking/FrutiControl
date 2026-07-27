a3aa5e45f67d410baae781197a9d8b154e5edf5d
tronstheking <javierenriquetrons@gmail.com>
2026-07-27 00:13:43 -0400

    Updates

--- assets/SuppliersPage-BSXaqHQn.js
+++ assets/SuppliersPage-BSXaqHQn.js
@@ -1,4 +1,4 @@
-import{c as i,R as c,u as t,j as e,h as l,P as d,i as x,e as m}from"./index-Cs1273H4.js";import{P as h}from"./pen-line-2E5EYc5m.js";/**
+import{c as i,R as c,u as t,j as e,h as l,P as d,i as x,e as m}from"./index-trG2b_7y.js";import{P as h}from"./pen-line-nskC5Nmo.js";/**
  * @license lucide-react v0.417.0 - ISC
  *
  * This source code is licensed under the ISC license.
