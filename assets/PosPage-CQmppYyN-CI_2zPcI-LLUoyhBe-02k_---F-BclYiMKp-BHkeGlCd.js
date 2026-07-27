3d618197e5b7c4261e594e8257f1419316c022b7
tronstheking <javierenriquetrons@gmail.com>
2026-07-26 23:00:30 -0400

    Updates

---
+++ assets/PosPage-CQmppYyN-CI_2zPcI-LLUoyhBe-02k_---F-BclYiMKp-BHkeGlCd.js
@@ -0,0 +1,1 @@
+import{g as r,y as t,D as a}from"./index-DRY-AhPg.js";const p=()=>{const e=r(s=>s.openModal),o=t();return a.useEffect(()=>{e("pos"),o("/",{replace:!0})},[e,o]),null};export{p as PosPage};
--- assets/shopping-cart-BYuc3Sen-BiBg0Gkf-Bk6c_Smj-CWESMvGY-BrDYL1Gn-DwFyYduv.js
+++ assets/shopping-cart-BYuc3Sen-BiBg0Gkf-Bk6c_Smj-CWESMvGY-BrDYL1Gn-DwFyYduv.js
@@ -1,4 +1,4 @@
-import{G as c}from"./index-D5M3Va-0.js";/**
+import{G as c}from"./index-DRY-AhPg.js";/**
  * @license lucide-react v0.417.0 - ISC
  *
  * This source code is licensed under the ISC license.
